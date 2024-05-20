da3cbbfb544cf20a4ce786489afa581508d908a5
swfkdlvm <80616036+swfkdlvm@users.noreply.github.com>
2024-05-20 11:55:29 +0900

    Delete store directory

--- store/src/main/java/com/store/jinyoung/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.jinyoung.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.store.jinyoung.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-	UserEntity findByuserEmail(String userEmail);
-}
